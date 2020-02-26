e8c743882417bc5f0ba53042930d52d342999e1c
Geodude25trade <rockpounder@gmail.com>
2020-02-26 16:39:53 -0700

    added return

--- textcleaner.py
+++ textcleaner.py
@@ -5,8 +5,4 @@ class TextCleaner:
 
     @staticmethod
     def letters(text, replace=" "):
-        re.sub(r"[^a-zA-Z]+", replace, text)
-
-    @staticmethod
-    def numbers(text, replace=" "):
-        re.sub(r"[^a-zA-Z]+", replace, text)
+        return re.sub(r"[^a-zA-Z]+", replace, text)
